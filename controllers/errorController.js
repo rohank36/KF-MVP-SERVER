8fedb8d8fad3707b9d54fbb37513cdb9c8b9ac8c
rohank36 <rohan.kanti@mail.mcgill.ca>
2023-08-15 16:19:35 -0400

    Testing error messages in prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -50,6 +50,10 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
+  if (process.env.NODE_ENV === "production") {
+    sendErrorDev(err, res);
+  }
+  /*
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
@@ -64,4 +68,5 @@ module.exports = (err, req, res, next) => {
 
     sendErrorProd(error, res);
   }
+  */
 };
