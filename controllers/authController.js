3d61b3cde1c15f0f372a37a5116fd5171a18a4fd
rohank36 <rohan.kanti@mail.mcgill.ca>
2023-08-15 16:34:56 -0400

    Removed console logs for whitelist testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,9 +43,6 @@ exports.betaUserCheck = catchAsync(async (req, res, next) => {
   const email = req.body.email;
   const isBetaUser = await whitelistChecker.isEmailWhitelisted(email);
 
-  console.log("Whitelisted Emails:", whitelistedEmails);
-  console.log("Checking Email:", email);
-
   if (!isBetaUser) {
     return next(new AppError("KaizenFlo only open for beta users", 401));
   }
